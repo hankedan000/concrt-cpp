09d5a84f7be8b8bcb16dca29ca0626ce06036ad2
Dan Hankewycz <dany32412@gmail.com>
2023-05-26 12:33:49 -0400

    Initial commit

---
+++ src/Math.cpp
@@ -0,0 +1,38 @@
+#include "templatelib/Math.h"
+
+namespace MyLib
+{
+
+int
+add(
+	int a,
+	int b)
+{
+	return a + b;
+}
+
+int
+sub(
+	int a,
+	int b)
+{
+	return a - b;
+}
+
+int
+mult(
+	int a,
+	int b)
+{
+	return a * b;
+}
+
+int
+div(
+	int a,
+	int b)
+{
+	return a / b;
+}
+
+}
